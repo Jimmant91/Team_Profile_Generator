b59fd0f3f08eae5e4252ea41771e6b8eb11b9311
James Antley <jimmy.antley@gmail.com>
2021-06-07 23:05:00 -0400

    finished employee js library

---
+++ HTMLsrc/html-template.js
@@ -0,0 +1,12 @@
+//Creating the employee team
+const generateTeam = team => {
+
+    //Manageer
+    const generateManager = manager => {
+        return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.get}
+        `
+    }
+}
